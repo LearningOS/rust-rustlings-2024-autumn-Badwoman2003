649d8a466bd552cf90c0dfade2f5ad1777a5e34e
Badwoman <3242204470@qq.com>
2024-09-23 15:08:57 +0800

    update: pass strings test

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,24 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let s = String::from(input.trim());
+    s
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let world = String::from(" world!");
+    let new_string = format!("{}{}",input,world);
+    new_string
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars","balloons")
+    
 }
 
 #[cfg(test)]
